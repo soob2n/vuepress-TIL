6dd78f23c8d98055a32553a766b3242b1b55deab
soobin <wwwbyn@naver.com>
2022-03-31 20:59:20 +0900

    commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,83 @@
+const { description } = require('../../package')
+
+module.exports = {
+  base: '/vuepress-TIL/',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Vuepress Docs Boilerplate',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: [
+      '/intro.md',
+      {
+        title: 'Javascript',
+        collapsable: true,
+        children: ['javascript/es6']
+      },
+      {
+        title: 'React',
+        collapsable: true,
+        children: ['react/jsx']
+      }
+    ]
+  },
+
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    'reading-progress',
+    '@vuepress/plugins-back-to-top',
+    'vuepress-plguin-smooth-scroll',
+    'vuepress-plugin-code-copy',
+      {
+        backgroundColor: '#42b883',
+        color: '#42b883',
+        backgroundTransition: true,
+        successText: 'copied!',
+        staticIcon: false,
+      },
+
+  ]
+}
